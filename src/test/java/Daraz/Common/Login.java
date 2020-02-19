34cda3b838c0e6b661da7f7b44807e7a9cf26a52
mrigesh singh <mrigeshsingh@hotmail.com>
2020-02-19 19:01:49 +0545

    moved parser file to MyDOMXMLParser

--- src/test/java/Daraz/Common/Login.java
+++ src/test/java/Daraz/Common/Login.java
@@ -3,6 +3,7 @@ package Daraz.Common;
 import Daraz.pageObjects.LandingPage;
 import Daraz.pageObjects.LoginPage;
 import org.openqa.selenium.WebDriver;
+import resources.MyDOMXMLParser;
 import resources.base;
 
 
@@ -12,19 +13,26 @@ public class Login extends base {
     LoginPage loginPage;
     public WebDriver driver;
     Waits wait;
+    MyDOMXMLParser parser;
 
     public Login(WebDriver driver) {
         this.driver = driver;
         landingPage = new LandingPage(driver);
         loginPage = new LoginPage(driver);
         wait = new Waits(driver);
+        parser = new MyDOMXMLParser();
     }
 
     public void userLogin() {
+
         landingPage.getLoginBtn().click();
-        loginPage.getUsername().sendKeys("9841558034");
-        loginPage.getPassword().sendKeys("maestro@222");
+        String username = parser.getUsernameOrPassword("username");
+        String password = parser.getUsernameOrPassword("password");
+        loginPage.getUsername().sendKeys(username);
+        loginPage.getPassword().sendKeys(password);
         loginPage.getLogin().click();
+
         wait.waitUntiltextPresentInElemet(landingPage.getBtnAfterLogin(),"ACCOUNT");
+
     }
 }
